cd415cba21ef6749adf9399173c6d6324fafb89b
Bijoy Kar <bijoykar54321@gmail.com>
2026-07-31 12:08:05 +0530

    update: add next-sitemap configuration and integrate into build process

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.myloandesk.com",
+  generateRobotsTxt: true,
+  outDir: "out", // Points to Next.js static output folder
+};
