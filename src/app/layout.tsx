44f07a1cb5403cf6b2795ff82c1752ca64e0f5e7
Bijoy Kar <bijoykar54321@gmail.com>
2026-08-05 15:20:34 +0530

    update: remove Google Analytics and update Microsoft Clarity script in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
-import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 import SiteHeader from "@/components/common/SiteHeader";
 import SiteFooter from "@/components/common/SiteFooter";
@@ -59,7 +59,7 @@ export default function RootLayout({
         />
 
         {/* Microsoft Clarity */}
-        <Script
+        {/* <Script
           id="microsoft-clarity"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
@@ -68,15 +68,12 @@ export default function RootLayout({
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "xxj3anvhgr");
+              })(window, document, "clarity", "script", "xxjjkwpvdd");
             `,
           }}
-        />
+        /> */}
 
-        {/* Google Analytics 4 */}
-        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID || "G-CJ020K1251"} />
-
-        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID || "GTM-M3N83T74"} />
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID || "GTM-KQ5H354K"} />
       </body>
     </html>
   );
